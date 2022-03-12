548ebb332d7666af3f08453df6d949a0c842fb20
alelee93 <leevargasale@gmail.com>
2022-03-11 19:51:51 -0600

    added challenge code

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,64 @@
+
+-- ****************************************** 
+-- CHALLENGE 1
+-- ****************************************** 
+
+
+-- (1) CREATE A RETIREMENT TITELS TABLE 
+SELECT ti.emp_no, em.first_name, em.last_name, ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees AS em
+JOIN titles as ti on em.emp_no = ti.emp_no
+WHERE em.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY em.emp_no;
+
+
+-- ****************************************** 
+-- (2) CREATE UNIQUE TITLES TABLE 
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+
+-- ****************************************** 
+-- (3) CREATE A RETIRING TITLES TABLE 
+-- Retrieve the number of employees by their most recent job title who are about to retire
+SELECT count(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+-- View reiring_titles table
+SELECT * from retiring_titles;
+
+
+ 
+-- ******************************************
+-- CHALLENGE 2
+-- ****************************************** 
+
+SELECT DISTINCT ON (emp_no) em.emp_no, 
+em.first_name, 
+em.last_name, 
+em.birth_date, 
+de.from_date, 
+de.to_date, 
+ti.title
+INTO mentorship_eligibility
+FROM employees AS em
+JOIN dept_emp AS de ON em.emp_no = de.emp_no
+JOIN titles AS ti ON em.emp_no = ti.emp_no
+WHERE de.to_date = '9999-01-01'
+AND em.birth_date BETWEEN '01-01-1965' AND '12-31-1965'
+ORDER BY em.emp_no
+
+SELECT * from mentorship_eligibility
+
